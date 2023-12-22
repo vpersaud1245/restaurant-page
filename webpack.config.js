a9cb101987e8f21968df8f0a88a0972d8426e0c8
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-21 19:28:18 -0500

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -6,12 +7,30 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant Page",
+      template: "src/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },

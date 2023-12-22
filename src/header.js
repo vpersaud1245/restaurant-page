7be6d8c25de9583c1199ac4efc4307fba27fc198
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-22 18:50:30 -0500

    Add header.js module to generate header

---
+++ src/header.js
@@ -0,0 +1,31 @@
+const createHeader = () => {
+  // CREATE HEADER ELEMENTS
+  const header = document.createElement("header");
+  const nav = document.createElement("nav");
+  const homeButton = document.createElement("button");
+  const menuButton = document.createElement("button");
+  const contactButton = document.createElement("button");
+
+  // ADD ELEMENT CLASSES
+  header.classList.add("header");
+  homeButton.classList.add("nav__button");
+  homeButton.classList.add("nav__button--home-button");
+  menuButton.classList.add("nav__button");
+  menuButton.classList.add("nav__button--menu-button");
+  contactButton.classList.add("nav__button");
+  contactButton.classList.add("nav__button--contact-button");
+
+  // ADD TEXT TO NAV BUTTONS
+  homeButton.textContent = "Home";
+  menuButton.textContent = "Menu";
+  contactButton.textContent = "Contact";
+
+  // APPEND ELEMENTS TO DOM
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+  header.appendChild(nav);
+  document.body.appendChild(header);
+};
+
+export { createHeader };
--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import "./style.css";
+import { createHeader } from "./header";
 import saladImg from "./assets/salad-hero-img.png";
 import gitHubLogo from "./assets/github-mark.svg";
 import gitHubLogoHover from "./assets/github-mark-hover.svg";
 
-const saladHeroImg = document.createElement("img");
-saladHeroImg.src = saladImg;
-saladHeroImg.classList.add("home__hero-img");
+createHeader();
 
-const homeContainer = document.querySelector(".home-container");
-homeContainer.append(saladHeroImg);
+// const saladHeroImg = document.createElement("img");
+// saladHeroImg.src = saladImg;
+// saladHeroImg.classList.add("home__hero-img");
 
-const gitHubLogoLink = document.createElement("a");
-gitHubLogoLink.classList.add("footer__git-hub-link");
-gitHubLogoLink.href = "https://github.com/vpersaud1245";
-gitHubLogoLink.target = "_blank";
+// const homeContainer = document.querySelector(".home-container");
+// homeContainer.append(saladHeroImg);
 
-const gitHubLogoImg = document.createElement("img");
-gitHubLogoImg.src = gitHubLogo;
-gitHubLogoImg.classList.add("git-hub-link__img");
-gitHubLogoLink.append(gitHubLogoImg);
+// const gitHubLogoLink = document.createElement("a");
+// gitHubLogoLink.classList.add("footer__git-hub-link");
+// gitHubLogoLink.href = "https://github.com/vpersaud1245";
+// gitHubLogoLink.target = "_blank";
 
-const gitHubLogoImgHover = document.createElement("img");
-gitHubLogoImgHover.src = gitHubLogoHover;
-gitHubLogoImgHover.classList.add("git-hub-link__img-hover");
-gitHubLogoLink.append(gitHubLogoImgHover);
+// const gitHubLogoImg = document.createElement("img");
+// gitHubLogoImg.src = gitHubLogo;
+// gitHubLogoImg.classList.add("git-hub-link__img");
+// gitHubLogoLink.append(gitHubLogoImg);
 
-const footer = document.querySelector(".footer");
-footer.append(gitHubLogoLink);
+// const gitHubLogoImgHover = document.createElement("img");
+// gitHubLogoImgHover.src = gitHubLogoHover;
+// gitHubLogoImgHover.classList.add("git-hub-link__img-hover");
+// gitHubLogoLink.append(gitHubLogoImgHover);
+
+// const footer = document.querySelector(".footer");
+// footer.append(gitHubLogoLink);

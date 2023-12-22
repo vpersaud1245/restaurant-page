44f2f5f3e4a10511bbd986c88949b1ad43aa20f0
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-22 18:35:11 -0500

    Style footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import "./style.css";
 import saladImg from "./assets/salad-hero-img.png";
+import gitHubLogo from "./assets/github-mark.svg";
+import gitHubLogoHover from "./assets/github-mark-hover.svg";
 
-let saladHeroImg = document.createElement("img");
+const saladHeroImg = document.createElement("img");
 saladHeroImg.src = saladImg;
 saladHeroImg.classList.add("home__hero-img");
 
-let homeContainer = document.querySelector(".home-container");
+const homeContainer = document.querySelector(".home-container");
 homeContainer.append(saladHeroImg);
+
+const gitHubLogoLink = document.createElement("a");
+gitHubLogoLink.classList.add("footer__git-hub-link");
+gitHubLogoLink.href = "https://github.com/vpersaud1245";
+gitHubLogoLink.target = "_blank";
+
+const gitHubLogoImg = document.createElement("img");
+gitHubLogoImg.src = gitHubLogo;
+gitHubLogoImg.classList.add("git-hub-link__img");
+gitHubLogoLink.append(gitHubLogoImg);
+
+const gitHubLogoImgHover = document.createElement("img");
+gitHubLogoImgHover.src = gitHubLogoHover;
+gitHubLogoImgHover.classList.add("git-hub-link__img-hover");
+gitHubLogoLink.append(gitHubLogoImgHover);
+
+const footer = document.querySelector(".footer");
+footer.append(gitHubLogoLink);

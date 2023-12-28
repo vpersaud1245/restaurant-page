9cf26a0b5c43b1aed3900af725bd5f3f6ba7c866
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-28 14:52:45 -0500

    Add hover event listner to github link

--- src/footer.js
+++ src/footer.js
@@ -11,8 +11,14 @@ const renderFooter = () => {
   // ADD ELEMENT CLASSES
   footer.classList.add("footer");
   gitHubLink.classList.add("footer__git-hub-link");
-  gitHubLogoImg.classList.add("git-hub-link__img");
-  gitHubLogoImgHover.classList.add("git-hub-link__img--hover");
+  gitHubLogoImg.classList.add(
+    "git-hub-link__img",
+    "git-hub-link__img--non-hover"
+  );
+  gitHubLogoImgHover.classList.add(
+    "git-hub-link__img",
+    "git-hub-link__img--hover"
+  );
 
   // LINK GITHUB PROFILE TO ANCHOR ELEMENT
   gitHubLink.href = "https://github.com/vpersaud1245";
@@ -30,6 +36,28 @@ const renderFooter = () => {
   // & FOOTER TO BODY
   footer.append(gitHubLink);
   document.body.append(footer);
+
+  // ADD HOVER EVENT LISTNER TO GITHUB LINK
+  gitHubLink.onmouseover = (e) => {
+    handleLinkHover(e, gitHubLogoImg, gitHubLogoImgHover);
+  };
+};
+
+/*
+  ----- EVENT LISTENER FUNCTIONS -----
+*/
+const handleLinkHover = (e, logoImg, logoHoverImg) => {
+  // MAKE HOVER IMG SHOW AND MOVE UP
+  logoImg.style.opacity = "0";
+  logoHoverImg.style.transform = "translateY(-5px)";
+
+  // ADD EVENT LISTENER TO FIRE WHEN HOVER IS COMPLETE
+  e.target.onmouseleave = (e) => {
+    logoHoverImg.style.transform = "translateY(0)";
+    setTimeout(() => {
+      logoImg.style.opacity = "100";
+    }, 200);
+  };
 };
 
 export { renderFooter };

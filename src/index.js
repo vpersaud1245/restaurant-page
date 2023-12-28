5e03838e8872bbb59758f69c71244f9a023f0634
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-28 14:30:19 -0500

    Change primary txt color for better contrast

--- src/index.js
+++ src/index.js
@@ -12,9 +12,5 @@ renderFooter();
 /** TO DO
  * Add github icon hover animation
  * Rename HTML title in webpack config to "Restaurant Page" when complete
- * Add menu to renderTab function on navButton event listener
- * Rebrand to Ramen
- * Add hover effect to menu-item
- * Add best seller and cut cost features
  * Add fade out to page
  */

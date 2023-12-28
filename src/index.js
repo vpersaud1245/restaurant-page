36259d7b2557e65f0cbb6ff1ae13de9f30fab81a
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-12-28 14:57:45 -0500

    Adjust header margin and menu gap

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ renderHomePage();
 renderFooter();
 
 /** TO DO
- * Add github icon hover animation
  * Rename HTML title in webpack config to "Restaurant Page" when complete
  * Add fade out to page
  */
